d188a8b597451fceccc11728c0e0282c7b43748b
htuncel <tuncel.halit@hotmail.com>
2021-03-13 15:15:54 +0100

    cors change

--- src/context.js
+++ src/context.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 
-const Context = React.createContext()
+const Context = React.createContext();
 function reducer(state, action) {
   switch (action.type) {
-    case 'SEARCH_TRACKS':
+    case "SEARCH_TRACKS":
       return {
         ...state,
         track_list: action.payload,
-        heading: 'Search Results'
-      }
+        heading: "Search Results",
+      };
 
     default:
-      return state
+      return state;
   }
 }
 
 export class Provider extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       track_list: [],
-      heading: 'Top 10 Tracks',
-      dispatch: action => this.setState(state => reducer(state, action))
-    }
+      heading: "Top 10 Tracks",
+      dispatch: (action) => this.setState((state) => reducer(state, action)),
+    };
   }
 
   componentDidMount() {
     axios
       .get(
-        `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
-          process.env.REACT_APP_MM_KEY
-        }`
+        `https://evening-refuge-58060.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${process.env.REACT_APP_MM_KEY}`
       )
-      .then(result => {
-        this.setState({ track_list: result.data.message.body.track_list })
+      .then((result) => {
+        this.setState({ track_list: result.data.message.body.track_list });
       })
-      .catch(error => console.error(error))
+      .catch((error) => console.error(error));
   }
 
   render() {
@@ -44,8 +42,8 @@ export class Provider extends Component {
       <Context.Provider value={this.state}>
         {this.props.children}
       </Context.Provider>
-    )
+    );
   }
 }
 
-export const Consumer = Context.Consumer
+export const Consumer = Context.Consumer;
